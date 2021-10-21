444196c0ca0530dd15c1c141a323d66a4d811406
Aparna Bhagwat <aparnabhagwat700@gmail.com>
2021-10-21 14:19:39 +0530

    second commit

--- some.java
+++ some.java
@@ -1,6 +1,7 @@
 public class some{
         public static void main(String args[]){
             System.out.println("Hello World");
+            System.out.println("Newly added statement");
         }
 
 }
